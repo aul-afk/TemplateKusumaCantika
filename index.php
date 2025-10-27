eef2ac2bb7c40578ca92fadafdc0d9edef6791f0
Ammar Tubagus <cantikakusuma370@gmail.com>
2025-10-27 21:19:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<body>
+	<?php include 'header.php';?>
+	<!--
+    Slider start
+    ============================== -->
+	<section id="slider">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="block wow fadeInUp" data-wow-duration="500ms" data-wow-delay="300ms">
+						<div class="title">
+							<h3>Featured <span>Works</span></h3>
+						</div>
+						<div id="owl-example" class="owl-carousel">
+							<div>
+								<img class="img-responsive" src="../assets/images/slider/slider-img-1.jpg" alt="">
+							</div>
+							<div>
+								<img class="img-responsive" src="../assets/images/slider/slider-img-2.jpg" alt="">
+							</div>
+							<div>
+								<img class="img-responsive" src="../assets/images/slider/slider-img-3.jpg" alt="">
+							</div>
+							<div>
+								<img class="img-responsive" src="../assets/images/slider/slider-img-4.jpg" alt="">
+							</div>
+							<div>
+								<img class="img-responsive" src="../assets/images/slider/slider-img-1.jpg" alt="">
+							</div>
+							<div>
+								<img class="img-responsive" src="../assets/images/slider/slider-img-2.jpg" alt="">
+							</div>
+							<div>
+								<img class="img-responsive" src="../assets/images/slider/slider-img-3.jpg" alt="">
+							</div>
+							<div>
+								<img class="img-responsive" src="../assets/images/slider/slider-img-4.jpg" alt="">
+							</div>
+
+						</div>
+					</div>
+				</div><!-- .col-md-12 close -->
+			</div><!-- .row close -->
+		</div><!-- .container close -->
+	</section><!-- slider close -->
+	<!--
+    about-us start
+    ============================== -->
+	<section id="about-us">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="block">
+						<img class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="400ms" src="../assets/images/cooker-img.png"
+							alt="cooker-img">
+						<h1 class="heading wow fadeInUp" data-wow-duration="400ms" data-wow-delay="500ms">Your
+							<span>Restaurant’s</span> </br> Website Has To Look <span>Good</span>
+						</h1>
+						<p class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="600ms">Lorem ipsum dolor sit
+							amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+							magna aliqua. Ut enim </br> ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+							ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in </br>voluptate
+							velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat</p>
+					</div>
+				</div><!-- .col-md-12 close -->
+			</div><!-- .row close -->
+		</div><!-- .containe close -->
+	</section><!-- #call-to-action close -->
+	<!--
+    blog start
+    ============================ -->
+	<section id="blog">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="block">
+						<h1 class="heading">Latest <span>From</span> the <span>Blog</span></h1>
+						<ul>
+							<li class="wow fadeInLeft" data-wow-duration="300ms" data-wow-delay="300ms">
+								<div class="blog-img">
+									<img src="../assets/images/blog/blog-img-1.jpg" alt="blog-img">
+								</div>
+								<div class="content-right">
+									<h3>Homestyle Chicken Pot Pie</h3>
+									<p>Prepared in true New England fash-ion. Tender all-white meat chicken simmered...
+									</p>
+								</div>
+							</li>
+							<li class="wow fadeInLeft" data-wow-duration="300ms" data-wow-delay="400ms">
+								<div class="blog-img">
+									<img src="../assets/images/blog/blog-img-2.jpg" alt="blog-img">
+								</div>
+								<div class="content-right">
+									<h3>Homestyle Chicken Pot Pie</h3>
+									<p>Prepared in true New England fash-ion. Tender all-white meat chicken simmered...
+									</p>
+								</div>
+							</li>
+							<li class="wow fadeInLeft" data-wow-duration="300ms" data-wow-delay="500ms">
+								<div class="content-left">
+									<h3>Homestyle Chicken Pot Pie</h3>
+									<p>Prepared in true New England fash-ion. Tender all-white meat chicken simmered...
+									</p>
+								</div>
+								<div class="blog-img-2">
+									<img src="../assets/images/blog/blog-img-3.jpg" alt="blog-img">
+								</div>
+							</li>
+							<li class="wow fadeInLeft" data-wow-duration="300ms" data-wow-delay="600ms">
+								<div class="content-left">
+									<h3>Homestyle Chicken Pot Pie</h3>
+									<p>Prepared in true New England fash-ion. Tender all-white meat chicken simmered...
+									</p>
+								</div>
+								<div class="blog-img-2">
+									<img src="../assets/images/blog/blog-img-4.jpg" alt="blog-img">
+								</div>
+							</li>
+							<li class="wow fadeInLeft" data-wow-duration="300ms" data-wow-delay="700ms">
+								<div class="blog-img">
+									<img src="../assets/images/blog/blog-img-5.jpg" alt="blog-img">
+								</div>
+								<div class="content-right">
+									<h3>Homestyle Chicken Pot Pie</h3>
+									<p>Prepared in true New England fash-ion. Tender all-white meat chicken simmered...
+									</p>
+								</div>
+							</li>
+							<li class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="800ms">
+								<div class="blog-img">
+									<img src="../assets/images/blog/blog-img-6.jpg" alt="blog-img">
+								</div>
+								<div class="content-right">
+									<h3>Homestyle Chicken Pot Pie</h3>
+									<p>Prepared in true New England fash-ion. Tender all-white meat chicken simmered...
+									</p>
+								</div>
+							</li>
+						</ul>
+						<a class="btn btn-default btn-more-info wow bounceIn" data-wow-duration="500ms" data-wow-delay="1200ms"
+							href="#" role="button">More Info</a>
+					</div>
+				</div><!-- .col-md-12 close -->
+			</div><!-- .row close -->
+		</div><!-- .containe close -->
+	</section><!-- #blog close -->
+	<!--
+    price start
+    ============================ -->
+	<section id="price">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="block">
+						<h1 class="heading wow fadeInUp" data-wow-duration="300ms" data-wow-delay="300ms">our
+							<span>MENU</span> the <span>PRICE</span></h1>
+						<p class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="400ms">Lorem ipsum dolor sit
+							amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+							magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco </p>
+						<div class="pricing-list">
+							<div class="title">
+								<h3>Featured <span>on the week</span></h3>
+							</div>
+							<ul>
+								<li class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="300ms">
+									<div class="item">
+										<div class="item-title">
+											<h2>Pancakes n' Such</h2>
+											<div class="border-bottom"></div>
+											<span>$ 25.00</span>
+										</div>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim&&</p>
+									</div>
+								</li>
+								<li class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="400ms">
+									<div class="item">
+										<div class="item-title">
+											<h2>Homestyle Chicken Pot Pie</h2>
+											<div class="border-bottom"></div>
+											<span>$ 10.00</span>
+										</div>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim&&</p>
+									</div>
+								</li>
+								<li class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="500ms">
+									<div class="item">
+										<div class="item-title">
+											<h2>Cereal, Panecillos y Frutas </h2>
+											<div class="border-bottom"></div>
+											<span>$ 5.00</span>
+										</div>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim&&</p>
+									</div>
+								</li>
+								<li class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="600ms">
+									<div class="item">
+										<div class="item-title">
+											<h2>Meat of Skewers</h2>
+											<div class="border-bottom"></div>
+											<span>$ 15.00</span>
+										</div>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim&&</p>
+									</div>
+								</li>
+								<li class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="700ms">
+									<div class="item">
+										<div class="item-title">
+											<h2>Steak with a Garlic and Parsley Risotto</h2>
+											<div class="border-bottom"></div>
+											<span>$ 75.00</span>
+										</div>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim&&</p>
+									</div>
+								</li>
+								<li class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="800ms">
+									<div class="item">
+										<div class="item-title">
+											<h2>Caesar Salad</h2>
+											<div class="border-bottom"></div>
+											<span>$ 7.00</span>
+										</div>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim&&</p>
+									</div>
+								</li>
+							</ul>
+							<a class="btn btn-default pull-right wow bounceIn" data-wow-duration="500ms" data-wow-delay="1200ms"
+								href="#" role="button">More Info</a>
+						</div>
+					</div>
+				</div><!-- .col-md-12 close -->
+			</div><!-- .row close -->
+		</div><!-- .containe close -->
+	</section><!-- #price close -->
+	<?php include 'contact.php';?>
+	<?php include 'footer.php';?>
+</html>
