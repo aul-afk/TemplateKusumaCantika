0ab9621400d24a0dc7e3a9acd3483aa97623d69c
aul-afk <e41251544@student.polije.ac.id>
2025-12-02 14:56:42 +0700

    Delete dbadmin/admin fx directory

--- dbadmin/admin fx/delete_check.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'config.php';
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM checks WHERE check_id = $id");
-header("Location: check_table.php");
-exit;
-?>
--- dbadmin/admin fx/delete_costume.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'config.php';
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM costume WHERE costume_id = '$id'");
-header("Location: kostum_table.php");
-exit;
-?>
